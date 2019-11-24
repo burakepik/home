aad3c18fd1d8ef4a44d1477f2b7296886ba06973
burakepik <burakepik@gmail.com>
2019-11-24 21:32:43 +0300

    Updates

---
+++ precache-manifest.524afc320a8de16acec6416efbd5a6af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5bcee1566758f7b5d6bccb218e751a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "40e2eb6861ec8e2194b7",
+    "url": "/home/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6917aba6b03eae0f6606",
+    "url": "/home/static/js/2.f5499dcc.chunk.js"
+  },
+  {
+    "revision": "40e2eb6861ec8e2194b7",
+    "url": "/home/static/js/main.5448ebe9.chunk.js"
+  },
+  {
+    "revision": "3cf7364bb1a73310b8d5",
+    "url": "/home/static/js/runtime-main.28c52758.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/home/static/media/logo.25bf045c.svg"
+  }
+]);
